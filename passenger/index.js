5b364935b803afef0bd9e95628849e9693b0bad0
alexia817 <alexiarodriguez817@gmail.com>
2023-01-24 12:42:17 -0500

    project

---
+++ passenger/index.js
@@ -0,0 +1,17 @@
+// document.getElementById("count").innerText = 5
+
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+}
+
